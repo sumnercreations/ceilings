176e23bbd97e49af7c224429e1baabd566cd1b83
Ammon Lockwood <ammon@plaidtie.net>
2017-10-15 19:16:17 -0600

    Feat: The grid is built.

--- src/app/velo-grid/velo-grid.component.ts
+++ src/app/velo-grid/velo-grid.component.ts
@@ -10,6 +10,7 @@ import { AlertService } from '../_services/alert.service';
 })
 export class VeloGridComponent implements OnInit {
   context:CanvasRenderingContext2D;
+  strokeStyle: string =  "#000000";
 
   @ViewChild("veloCanvas") canvas;
 
@@ -33,24 +34,64 @@ export class VeloGridComponent implements OnInit {
 
   updateGrid() {
     let canvas = this.canvas.nativeElement;
-    this.debug.log('velo-grid-component', canvas);
-    this.context = canvas.getContext("2d");
-    let numberOfSides = 6,
-      size = 25,
-      Xcenter = 25,
-      Ycenter = 25;
+    canvas.width = 820;
+    canvas.height = 500;
 
-    let ctx = this.context;
-    ctx.beginPath();
-    ctx.moveTo (Xcenter + size * Math.cos(0), Ycenter + size * Math.sin(0));
-
-    for (let i = 1; i <= numberOfSides; i++) {
-      ctx.lineTo (Xcenter + size * Math.cos(i * 2 * Math.PI / numberOfSides), Ycenter + size * Math.sin(i * 2 * Math.PI / numberOfSides));
-    }
-
-    ctx.strokeStyle = "#000000";
+    let ctx = canvas.getContext("2d");
     ctx.lineWidth = 1;
+
+    let rows = 10;
+    let columns = 9;
+    let adjustmentX = 96;
+    let adjustmentY = 48;
+
+    for (var r = 0; r < rows; ++r) {
+      for (var c = 0; c < columns; ++c) {
+        this.createPentagonSection(ctx, c * adjustmentX, r * adjustmentY, this.isOdd(r));
+      }
+    }
+  }
+
+  private createPentagonSection(ctx, adjustmentX, adjustmentY, isOdd) {
+    this.debug.log('velo-grid', isOdd);
+    if(isOdd) {
+      // start off 48px off canvas
+      this.drawPentagon(ctx, 18 + adjustmentX, 33 + adjustmentY, -Math.PI / 2);
+      this.drawPentagon(ctx, 50 + adjustmentX, 17 + adjustmentY, Math.PI);
+      this.drawPentagon(ctx, 50 + adjustmentX, 49 + adjustmentY, 2 * Math.PI);
+      this.drawPentagon(ctx, 82 + adjustmentX, 33 + adjustmentY, Math.PI / 2);
+    }else{
+      this.drawPentagon(ctx, -30 + adjustmentX, 33 + adjustmentY, -Math.PI / 2);
+      this.drawPentagon(ctx, 2 + adjustmentX, 17 + adjustmentY, Math.PI);
+      this.drawPentagon(ctx, 2 + adjustmentX, 49 + adjustmentY, 2 * Math.PI);
+      this.drawPentagon(ctx, 34 + adjustmentX, 33 + adjustmentY, Math.PI / 2);
+    }
+  }
+
+  private drawPentagon(ctx, x, y, rotateAngle) {
+    // save the current canvas state
+    ctx.save();
+
+    ctx.beginPath();
+    ctx.translate(x,y);
+    ctx.rotate(rotateAngle);
+    ctx.moveTo(0, 2*7.97);
+    ctx.lineTo(2*-11.95, 2*3.98);
+    ctx.lineTo(2*-7.975, 2*-7.97);
+    ctx.lineTo(2*7.975, 2*-7.97);
+    ctx.lineTo(2*11.95, 2*3.98);
+    ctx.closePath();
+    ctx.strokeStyle = this.strokeStyle;
     ctx.stroke();
+    ctx.restore();
+  }
+
+  private toRadians(angle) {
+    return angle * (Math.PI / 180);
+  }
+
+  private isOdd(column: number) {
+    return column % 2;
   }
 
 }
