ae714bf3feb480a09b8392343259c1baf8c34316
Andrew Crane <andrew.crane@3-form.com>
2018-01-31 08:25:30 -0700

    feat: adds hush-block-size image and rounding dimensions up to 24

--- src/app/options/hush-options/hush-options.component.ts
+++ src/app/options/hush-options/hush-options.component.ts
@@ -21,15 +21,17 @@ export class HushOptionsComponent extends OptionsComponent implements OnInit {
   }
 
   closeDialog() {
-    const newWidth = Math.floor(this.feature.width / 24) * 24;
-    const newLength = Math.floor(this.feature.length / 24) * 24;
+    let newWidth = Math.floor(this.feature.width / 24) * 24;
+    let newLength = Math.floor(this.feature.length / 24) * 24;
+    if (newWidth === 0) { newWidth = 24; this.alert.error(`Width rounded up to 24`); }
+    if (newLength === 0) { newLength = 24; this.alert.error(`Length rounded up to 24`); }
     if ((this.feature.width % 24 !== 0) && (this.feature.length % 24 !== 0)) {
       this.feature.width = newWidth;
       this.feature.length = newLength;
-      this.alert.error(`Width and Height rounded down to ${newWidth}x${newLength}`);
+      this.alert.error(`Width and Height rounded to ${newWidth}x${newLength}`);
     } else if (this.feature.width % 24 !== 0) {
       this.feature.width = newWidth;
-      this.alert.error(`Width rounded down ${newWidth}`);
+      this.alert.error(`Width rounded down to ${newWidth}`);
     } else if (this.feature.length % 24 !== 0) {
       this.feature.length = newLength;
       this.alert.error(`Height rounded down to ${newLength}`);
