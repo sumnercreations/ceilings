3fb1fd2a2adfe1d77e7f936793769b5b13bc80b3
Andrew Crane <andrew.crane@3-form.com>
2018-07-16 16:08:22 -0600

    feat: fixes the styled-button directive

--- src/app/_directives/styled-button.directive.ts
+++ src/app/_directives/styled-button.directive.ts
@@ -12,9 +12,10 @@ export class StyledButtonDirective implements AfterViewInit {
   ngAfterViewInit() {
     this.renderer.setStyle(this.el.nativeElement, 'display', 'flex');
     this.renderer.setStyle(this.el.nativeElement, 'align-items', 'center');
-    this.renderer.setStyle(this.el.nativeElement, 'min-width', '150px');
     this.renderer.setStyle(this.el.nativeElement, 'justify-content', 'space-between');
     const button = this.el.nativeElement.getElementsByTagName('button')[0];
+    this.renderer.setStyle(button, 'margin', '0px');
+    this.renderer.setStyle(button, 'min-width', '150px');
     this.renderer.setStyle(button, '-webkit-appearance', 'none');
     this.renderer.setStyle(button, '-moz-appearance', 'none');
     this.renderer.setStyle(button, 'appearance', 'none');
@@ -56,37 +57,40 @@ export class StyledButtonDirective implements AfterViewInit {
     this.renderer.setStyle(button, 'font-weight', '500');
     this.renderer.setStyle(button, 'text-indent', '8px');
     this.renderer.setStyle(button, 'background-color', '#000');
+    this.renderer.setStyle(button, 'border', '2px solid #000');
     this.renderer.setStyle(button, 'color', '#fff');
-    this.renderer.setStyle(button, 'padding-right', '30px');
   }
 
   styleSecondaryButton(button) {
     this.renderer.setStyle(button, 'background-color', '#fff');
+    this.renderer.setStyle(button, 'border', '2px solid #fff');
     this.renderer.setStyle(button, 'line-height', '3.75');
     this.renderer.setStyle(button, 'font-size', '0.9em');
     this.renderer.setStyle(button, 'font-weight', '500');
-    this.renderer.setStyle(button, 'padding-right', '30px');
   }
 
   addArrowRight(button) {
-    const newSpan = this.renderer.createElement('span');
+    const newImg = this.renderer.createElement('img');
     const arrowColor = this.buttonType.includes('primary') ? 'arrow-white' : 'arrow';
-    this.renderer.setStyle(newSpan, 'height', '18px');
-    this.renderer.setStyle(newSpan, 'width', '18px');
-    this.renderer.setStyle(newSpan, 'margin-right', '12px');
-    this.renderer.setStyle(newSpan, 'background', `url("/assets/icons/tools/${arrowColor}.png") no-repeat top left`);
-    this.renderer.setStyle(newSpan, 'background-size', 'contain');
-    this.renderer.appendChild(this.el.nativeElement, newSpan);
+    this.renderer.setAttribute(newImg, 'src', `/assets/icons/tools/${arrowColor}.png`);
+    this.renderer.setStyle(newImg, 'height', '18px');
+    this.renderer.setStyle(newImg, 'width', '18px');
+    this.renderer.setStyle(newImg, 'margin-right', '15px');
+    this.renderer.setStyle(newImg, 'margin-left', '30px');
+    this.renderer.appendChild(button, newImg);
   }
 
   addArrowLeft(button) {
-    const newSpan = this.renderer.createElement('span');
+    const newImg = this.renderer.createElement('img');
     const arrowColor = this.buttonType.includes('primary') ? 'arrow-white' : 'arrow';
-    this.renderer.setStyle(newSpan, 'height', '18px');
-    this.renderer.setStyle(newSpan, 'width', '18px');
-    this.renderer.setStyle(newSpan, 'background', `url("/assets/icons/tools/${arrowColor}.png") no-repeat top left`);
-    this.renderer.setStyle(newSpan, 'background-size', 'contain');
-    this.renderer.appendChild(this.el.nativeElement, newSpan);
-    this.renderer.setStyle(this.el.nativeElement, 'flex-direction', 'row-reverse');
+    this.renderer.setAttribute(newImg, 'src', `/assets/icons/tools/${arrowColor}.png`);
+    this.renderer.setStyle(newImg, 'height', '18px');
+    this.renderer.setStyle(newImg, 'width', '18px');
+    this.renderer.setStyle(newImg, 'margin-left', '15px');
+    this.renderer.setStyle(newImg, 'margin-right', '30px');
+    this.renderer.appendChild(button, newImg);
+    this.renderer.setStyle(button, 'display', 'flex');
+    this.renderer.setStyle(button, 'flex-direction', 'row-reverse');
+    this.renderer.setStyle(button, 'align-items', 'center');
   }
 }
